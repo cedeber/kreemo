44d002beb1fcc0a18ad3fa887b0a458c52978039
Cédric Eberhardt <hello+code@cedeber.fr>
2021-02-13 12:29:22 +0100

    migrate to vite

--- react-threejs-start/src/index.tsx
+++ react-threejs-start/src/index.tsx
@@ -11,7 +11,4 @@ ReactDOM.render(
 );
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
-if (import.meta.hot) {
-    import.meta.hot.accept();
-}
+import.meta.hot?.accept();
---
+++ react-threejs-start/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+export default defineConfig({
+    plugins: [reactRefresh()],
+    build: {
+        target: "esnext",
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ["react", "react-dom", "react-three-fiber", "@react-three/drei"],
+                    three: ["three"],
+                },
+            },
+        },
+        chunkSizeWarningLimit: 600, // default 500
+    },
+});
