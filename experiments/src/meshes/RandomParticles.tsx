1e0e877566ca08d8bd9254a8df4503dda6a05cbc
Cédric Eberhardt <hello+code@cedeber.fr>
2021-02-14 19:40:45 +0100

    update particles

--- experiments/src/meshes/RandomParticles.tsx
+++ experiments/src/meshes/RandomParticles.tsx
@@ -1,39 +1,75 @@
 import React, { useEffect, useRef, useState } from "react";
 import { PointsProps, useLoader } from "react-three-fiber";
-import { BufferAttribute, Points, TextureLoader } from "three";
+import { AdditiveBlending, BufferAttribute, Color, Points, TextureLoader } from "three";
 import { useDatGui } from "../hooks/useDatGui";
 import mapTexture from "../assets/circle_03.jpg";
 
 const RandomParticles: React.FC<PointsProps> = (props) => {
     const points = useRef<Points>();
-    const count = useDatGui("Particles", 10000, 0, 1000000, 100);
+
+    // settings
+    const count = useDatGui("Particles", 30000, 0, 100000, 100);
+    const size = useDatGui("Size", 0.05, 0.005, 0.3, 0.001);
+    const radius = useDatGui("Radius", 10, 1, 20, 0.1);
+    const branches = useDatGui("Branches", 3, 3, 9, 1);
+    const spin = useDatGui("Spin", 0.5, -5, 5, 0.001);
+    const random = useDatGui("Randomness", 0.5, 0.001, 1, 0.001);
+    const power = useDatGui("Power", 3, 1, 6, 0.01);
+
     const positions = useRef(new Float32Array(count * 3));
+    const colors = useRef(new Float32Array(count * 3));
     const [forceUpdate, setForceUpdate] = useState(0);
 
     const [map] = useLoader(TextureLoader, [mapTexture]);
 
     useEffect(() => {
         positions.current = new Float32Array(count * 3);
+        colors.current = new Float32Array(count * 3);
+
+        const insideColor = new Color("#ff6030");
+        const outsideColor = new Color("#1b3984");
+
         for (let i = 0; i < count; i++) {
-            positions.current[i] = (Math.random() - 0.5) * 10;
+            const i3 = i * 3;
+            const r = Math.random() * radius;
+            const s = r * spin;
+            const angle = ((i % branches) / branches) * Math.PI * 2;
+
+            const rdm = () =>
+                Math.pow(Math.random(), power) * (Math.random() < 0.5 ? 1 : -1) * random * r;
+
+            positions.current[i3] = rdm() * random + Math.cos(angle + s) * r;
+            positions.current[i3 + 1] = rdm() * random;
+            positions.current[i3 + 2] = rdm() * random + Math.sin(angle + s) * r;
+
+            const mixedColor = insideColor.clone();
+            mixedColor.lerp(outsideColor, r / radius);
+
+            colors.current[i3] = mixedColor.r;
+            colors.current[i3 + 1] = mixedColor.g;
+            colors.current[i3 + 2] = mixedColor.b;
         }
         setForceUpdate(forceUpdate + 1);
-    }, [count]);
+    }, [count, radius, branches, spin, random, power]);
 
     return (
         <points {...props} ref={points}>
             <bufferGeometry
                 attributes={{
                     position: new BufferAttribute(positions.current, 3),
+                    color: new BufferAttribute(colors.current, 3),
                 }}
             />
             <pointsMaterial
-                size={0.05}
+                size={size}
                 sizeAttenuation
-                color={0xffffff}
+                // color={0xffffff}
                 transparent
                 alphaMap={map}
-                alphaTest={0.01}
+                // alphaTest={0.01}
+                depthWrite={false}
+                blending={AdditiveBlending}
+                vertexColors
             />
         </points>
     );
